cb91434c68b8b2fdafa7b6674ea3eab33c4c63aa
pranpranpran <sushantaarya111@gmail.com>
2025-01-09 15:38:19 +0530

    all the file added

---
+++ src/main/java/org/example/mrityunjay/Alien.java
@@ -0,0 +1,11 @@
+package org.example.mrityunjay;
+
+public class Alien {
+
+    public Alien(){
+        System.out.println("Alien Object is created");
+    }
+    public void run(){
+        System.out.println("running");
+    }
+}
