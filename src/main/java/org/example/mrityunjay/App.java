664b6bce9825d7738466749fa077921238d578bf
pranpranpran <sushantaarya111@gmail.com>
2025-01-09 15:35:55 +0530

    updated

--- src/main/java/org/example/mrityunjay/App.java
+++ src/main/java/org/example/mrityunjay/App.java
@@ -1,5 +1,8 @@
 package org.example.mrityunjay;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Alien obj = (Alien) context.getBean("alien1");
+        obj.run();
     }
 }
---
+++ src/main/java/org/example/mrityunjay/Laptop.java
@@ -0,0 +1,7 @@
+package org.example.mrityunjay;
+public class Laptop{
+    public Laptop(){
+        System.out.println("Laptop obj is created");
+    }
+
+}
